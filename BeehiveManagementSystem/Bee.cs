e44a4bdfd738a87deaa02b60ac31dcc0db44729f
konstantintroetsky <konstantintroetsky@gmail.com>
2024-01-03 14:13:02 +1000

    Sec

---
+++ BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    internal class Bee
+    {
+        public virtual float CostPerShift { get; private set; }
+
+        public string Job { get; private set; }
+        
+       
+
+        public Bee(string job)
+        {
+            Job = job;
+        }
+
+
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+
+        protected virtual void DoJob() { }
+    }
+}
---
+++ BeehiveManagementSystem/HiveDefender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    class HiveDefender : Bee
+    {
+
+        public override float CostPerShift => base.CostPerShift;
+
+        protected override void DoJob()
+        {
+            base.DoJob();
+        }
+
+        public HiveDefender()
+            :base("Hive Defender")
+        {
+
+        }
+
+ 
+
+    }
+}
